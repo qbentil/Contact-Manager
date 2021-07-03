5329f1c271b2bcfcca00a9fb896faeccfa5780cf
Bentil Shadrack <bentilshadrack72@gmail.com>
2021-07-03 11:39:13 +0000

    Program Files

---
+++ contactManager/src/academy/Bentilzone/dbConfig.java
@@ -0,0 +1,20 @@
+package academy.Bentilzone;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class dbConfig {
+
+    public static Connection getConnection(){
+        Connection con = null;
+
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/contact_manager", "root", "");
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+
+        return con;
+    }
+}
